be437ca5c633e7880ac2f87cb0779ffbf4b693e7
ziginsider <ziginsider@gmail.com>
2018-05-28 16:36:33 +0300

    add LocationService::addOnCompleteListener()

--- app/src/main/java/io/github/ziginsider/epam_laba_13/LocationService.kt
+++ app/src/main/java/io/github/ziginsider/epam_laba_13/LocationService.kt
@@ -17,6 +17,7 @@ import com.google.android.gms.location.LocationResult
 import android.os.HandlerThread
 import android.app.NotificationChannel
 import android.os.Build
+import io.github.ziginsider.epam_laba_13.utils.loge
 
 class LocationService : Service() {
 
@@ -26,7 +27,7 @@ class LocationService : Service() {
     private var locationRequest: LocationRequest? = null
     private var locationCallback: LocationCallback? = null
     private var serviceHandler: Handler? = null
-    private val currentLocation: Location? = null
+    private var currentLocation: Location? = null
     private val binder = LocalBinder()
 
     override fun onCreate() {
@@ -74,6 +75,7 @@ class LocationService : Service() {
             private set
 
     }
+
     private fun createLocationRequest() {
         locationRequest = LocationRequest().apply {
             interval = UPDATE_INTERVAL_IN_MILLISECONDS
@@ -84,10 +86,19 @@ class LocationService : Service() {
 
     private fun addOnCompleteListener() {
         try {
-
+            fusedLocationClient?.lastLocation
+                    ?.addOnCompleteListener { task ->
+                        task.run {
+                            if (isSuccessful && result != null) {
+                                currentLocation = result
+                            } else {
+                                loge(TAG, "[ Failed to get location ]")
+                            }
+                        }
+                    }
         } catch (e: SecurityException) {
-
-            
+            loge(TAG, "[ Lost location permission: $e ]")
+            e.printStackTrace()
         }
     }
 
@@ -95,13 +106,12 @@ class LocationService : Service() {
 
         private val TAG = LocationService::class.java.simpleName
         private const val PACKAGE_NAME = "io.github.ziginsider.epam_laba_13"
-        private const val CHANNEL_ID = "channel_1"
+        private const val CHANNEL_ID = "channel_13"
         private const val ACTION_BROADCAST = "$PACKAGE_NAME.broadcast"
         private const val EXTRA_LOCATION = "$PACKAGE_NAME.location"
         private const val EXTRA_STARTED_FROM_NOTIFICATION = "$PACKAGE_NAME.started_from_notification"
         private const val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 10000
-        private const val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS
-                = UPDATE_INTERVAL_IN_MILLISECONDS / 2
+        private const val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2
         private const val NOTIFICATION_ID = 12345678
 
     }
